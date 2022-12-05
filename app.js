220d85a7c9697170eb8c6da78b5895123412100e
Matthew <mattez2009@gmail.com>
2022-12-05 04:22:59 +0100

    miniproject committed to github

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.getElementById('hamburger')
+cons navList = document.getElementById('navList')
+
+function = toggleButton() {
+	navlist.classList.toggle('show;)
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
